4214261e89aeb8e5508a13599dec8c86d63e8180
dinakidane <dinakidane@hotmail.com>
2023-11-29 16:26:49 +0000

    editing categories

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    //sidenav intitialisation 
+    var sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+  });
